e11a086764ab18f8236f9f129370819ae77534a4
LucyHorng <khmin09015@gmail.com>
2020-04-26 19:40:31 +0900

    프로파일 페이지 구현

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,5 +1,13 @@
 <template>
-    <v-content>
-        
-    </v-content>
+  <v-content>
+    <v-container class="fill-height" fluid>
+      <v-row align="center" justify="start">
+        <v-col cols="12" sm="8" md="4">
+          <v-avatar size="100">
+            <img src="../assets/lucy.jpg" alt="lucy" />
+          </v-avatar>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-content>
 </template>
