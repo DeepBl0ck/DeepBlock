b70bfa7daef038c6331c95554c8dcfc412a3b821
LucyHorng <khmin09015@gmail.com>
2020-05-03 00:45:27 +0900

    board vue 파일 생성

---
+++ src/views/Board.vue
@@ -0,0 +1,15 @@
+<template>
+    <di id="board">
+        <h1>This is Board Page</h1>
+    </di>
+</template>
+
+<script>
+    export default {
+        name: "Board"
+    }
+</script>
+
+<style scoped>
+
+</style>
