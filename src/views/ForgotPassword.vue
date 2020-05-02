e8614501f227fbc0b06c02be3a7ba2153c2c7f82
LucyHorng <khmin09015@gmail.com>
2020-05-03 03:38:10 +0900

    비밀번호 찾기 폼 구현

---
+++ src/views/ForgotPassword.vue
@@ -0,0 +1,51 @@
+<template>
+  <v-content>
+    <v-container class="fill-height" fluid>
+      <v-row align="center" justify="center">
+        <v-col cols="12" sm="8" md="4">
+          <v-card icon max-width="400">
+            <v-list-item-title style="font-size:1.5em; color: #7986CB;">
+              <div style="padding-top: 10px; padding-bottom: 10px;">
+                <v-icon large>mdi-view-headline</v-icon>DeepBlock
+              </div>
+            </v-list-item-title>
+            <v-divider color="#3949AB"></v-divider>
+
+            <v-card-text
+              style="font-size:1.2em; color: #7986CB;padding-top: 50px;"
+              >PASSWORD RECOVER</v-card-text
+            >
+
+            <v-form style="padding: 20px 50px 20px 50px">
+              <v-text-field
+                id="email"
+                label="Email"
+                outlined
+                dense
+              ></v-text-field>
+              <v-btn @click="submit" block dark color="indigo">Send password reset email </v-btn>
+              <v-layout
+                justify-space-between
+                class="rememberme"
+                style="padding: 10px 0px 0px 0px;"
+              >
+                <a href="/login" style="font-size:14px;color:black;text-decoration:none">return to Login</a>
+                <a href="/signUp" style="font-size:14px;color:black;text-decoration:none">Sign Up!</a>
+              </v-layout>
+            </v-form>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-content>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.rememberme .v-label {
+  font-size: 14px;
+}
+</style>
