c19018ac9d90805c4e48117aeeb2a80be7ea4c67
LucyHorng <khmin09015@gmail.com>
2020-05-10 13:29:28 +0900

    글자 색 변경, 주석 제거

--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -4,20 +4,15 @@
       <v-row align="center" justify="center">
         <v-col cols="12" sm="8" md="4">
           <v-card icon max-width="400">
-            <v-list-item-title style="font-size:1.5em; color: #7986CB;">
+            <v-list-item-title style="font-size:1.5em; color: #3949AB;">
               <div style="padding-top: 10px; padding-bottom: 10px;">
                 <v-icon large>mdi-view-headline</v-icon>DeepBlock
               </div>
             </v-list-item-title>
             <v-divider color="#3949AB"></v-divider>
 
-            <!-- <v-card-text
-              style="font-size:1.2em; color: #7986CB;padding-top: 50px;"
-              >Create your account</v-card-text
-            > -->
-
             <v-card-text
-              style="font-size:1.2em; color: #7986CB;padding-top: 50px;"
+              style="font-size:1.2em; color: #3949AB;padding-top: 50px;"
               >CREATE YOUR ACCOUNT</v-card-text
             >
 
