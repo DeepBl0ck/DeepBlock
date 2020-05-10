7dcb9707109db20ebaeee6d4e354ee8e58f615e7
jskimm <plumssi9@gmail.com>
2020-05-10 15:58:05 +0900

    adjust google font

--- src/App.vue
+++ src/App.vue
@@ -15,3 +15,25 @@ import Nevbar from '@/components/Nevbar'
     }
   }
 </script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Open+Sans&display=swap');
+#app {
+  /* font-family: Avenir, Helvetica, Arial, sans-serif; */
+  font-family: 'Open Sans', sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+#nav {
+  padding: 30px;
+}
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
