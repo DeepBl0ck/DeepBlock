365a94fa168ba2a6509a409a906a14118266133f
jskimm <plumssi9@gmail.com>
2020-05-10 20:13:56 +0900

    add to axios

--- src/main.js
+++ src/main.js
@@ -5,7 +5,10 @@ import store from './store'
 import vuetify from './plugins/vuetify';
 import Grid from "vue-js-grid"
 import VueInputAutowidth from 'vue-input-autowidth'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
+Vue.use(VueAxios, axios)
 Vue.use(VueInputAutowidth)
 Vue.use(Grid);
 Vue.config.productionTip = false
