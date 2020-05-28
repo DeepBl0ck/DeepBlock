af36f6e968d191e9f1676b8619690003f407e603
devsungmin <kimsungmin.dev@gmail.com>
2020-05-29 02:19:00 +0900

    add: add Left / Right Navi bar (Train Chart / Evaluation Chart)

---
+++ src/components/RightNavibar.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-navigation-drawer v-model="RightNavbar" right clipped app>
+    
+  </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+  name: "RightNavbar",
+  components: {
+  },
+  data() {
+    return {
+     
+    };
+  }
+};
+</script>
+
+<style lang="sass">
+
+</style>
