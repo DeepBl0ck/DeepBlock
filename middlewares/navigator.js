0ed8950367d0a0923de800f89db4034d216adeb1
jskimm <plumssi9@gmail.com>
2020-05-28 09:34:45 +0900

    refactor: middlewares

--- middlewares/navigator.js
+++ middlewares/navigator.js
@@ -1,22 +1,16 @@
 /*===================================
-  Get path where data will bd saved 
+  Get path where data will be saved 
 ====================================*/
 'use strict'
 
-//modules
-const crypto            = require('crypto');
-// models - DB  
-const models            = require("../models");
-// configs  
-const base_path         = require('../config/configs').base_path;
-const profile_dir_name  = require('../config/configs').profile_dir_name;
-const hash              = require('../config/configs').hash;
-const salt              = require('../config/configs').salt;
-// utils  
-const responseHandler   = require('../utils/responseHandler');
+const crypto = require('crypto');
+const models = require("@models");
+const path = require("@config/path")
+const responseHandler = require('@utils/responseHandler');
+const salt = process.env.SALT;
 
 module.exports = {
-  image(req, res, next){
+  image(req, res, next) {
     models.Dataset.findOne({
       include: [{
         model: models.Class,
@@ -25,30 +19,32 @@ module.exports = {
       where: {
         id: req.params.dataset_id
       }
-    }).then((exist) => {
-      if (exist) {
-        req.dataset_path = exist.dataValues.datasetPath;
-        req.original_path = exist.dataValues.Classes[0].dataValues.originalPath;
-        req.thumbnail_path = exist.dataValues.Classes[0].dataValues.thumbnailPath;
-  
-        console.log("navi success");
-        next();
-      } else {
-        console.log("navi failed");
-        responseHandler.fail(res, 400, "잘못 된 접근")
-      }
-    }).catch((err) => {
-      console.log(err);
-      responseHandler.fail(res, 500, "처리 실패");
     })
+      .then((exist) => {
+        if (exist) {
+          req.dataset_path = exist.dataValues.datasetPath;
+          req.original_path = exist.dataValues.Classes[0].dataValues.originalPath;
+          req.thumbnail_path = exist.dataValues.Classes[0].dataValues.thumbnailPath;
+
+          console.log("navi success");
+          return next();
+        } else {
+          console.log("navi failed");
+          responseHandler.fail(res, 400, "잘못 된 접근")
+        }
+      })
+      .catch((err) => {
+        console.log(err); 
+        responseHandler.fail(res, 500, "처리 실패");
+      })
   },
 
-  avatar(req, res, next){
-    const hashId = crypto.createHash(hash).update(req.session.username + salt).digest("hex");
-    req.user_path = `${base_path}/${hashId}/${profile_dir_name}/`;
+  avatar(req, res, next) {
+    const hashed_id = crypto.createHash("sha256").update(req.session.username + salt).digest("hex");
+    req.profile_path = `${path.storage}/${hashed_id}/${path.profile}/`;
     req.profile_name = req.session.username;
-  
-    next();
+
+    return next();
   }
 }
 
--- middlewares/sanitizer.js
+++ middlewares/sanitizer.js
@@ -7,27 +7,16 @@ var { check, validationResult } = require('express-validator');
 
 const sanitizer = (req, res, next) => {
   const errors = validationResult(req);
-
-  const p = new Promise(
-    (resolve, reject) => {
-      console.log(errors);
-      if (!errors.isEmpty()) {
-        reject(errors);
-      }
-      resolve();
-    }
-  )
-  p.then(() => {
-    console.log("sanitizer success");
-    next();
-  })
-    .catch(function (errors) {
-      console.log("sanitizer failed");
-      res.status(403).json({
-        success: false,
-        message: errors
-      });
+  console.log(errors);
+  if (!errors.isEmpty()) {
+    console.log("sanitizer failed");
+    return res.status(403).json({
+      success: false,
+      message: errors
     });
+  } 
+  console.log("sanitizer success");
+  return next();
 };
 
 module.exports = sanitizer;
