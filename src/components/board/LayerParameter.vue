96eeca6b234207f73831c0525b8d055fcd684b65
jinsung_cho <cjs97111@gmail.com>
2020-05-31 00:38:36 +0900

    Change navigation drawer to v-list

--- src/components/board/LayerParameter.vue
+++ src/components/board/LayerParameter.vue
@@ -1,42 +1,52 @@
 <template>
-  <v-navigation-drawer v-model="parameter" clipped right app color="#ECEFF1">
-    <div class="parameter" >
+  <v-list>
+    <div class="parameter">
       <h2>Parameter Input</h2>
       <!-- TODO: Vuex layserState 변수 사용해서 각각의 레이어에 맞게  파라미터 값 나오게 하기-->
-      <v-divider class="line"/>
+      <v-divider class="line" />
       <div class="inputParameter">
-        
-        <v-text><b>KernelSize</b></v-text>
+        <v-text>
+          <b>KernelSize</b>
+        </v-text>
         <v-text-field placeholder="ex) 5" />
 
-        <v-text><b>InputShape</b></v-text>
+        <v-text>
+          <b>InputShape</b>
+        </v-text>
         <v-text-field placeholder="ex) 28, 28, 1" />
 
-        <v-text><b>PoolingSize</b></v-text>
+        <v-text>
+          <b>PoolingSize</b>
+        </v-text>
         <v-text-field placeholder="ex) 28, 28" />
 
-        <v-text><b>Filter</b></v-text>
+        <v-text>
+          <b>Filter</b>
+        </v-text>
         <v-text-field placeholder="ex) 16" />
 
-        <v-text><b>Stride</b></v-text>
+        <v-text>
+          <b>Stride</b>
+        </v-text>
         <v-text-field placeholder="ex) 1" />
 
-        <v-text><b>Activation</b></v-text>
-        <v-text-field placeholder="ex) Relu"/>
+        <v-text>
+          <b>Activation</b>
+        </v-text>
+        <v-text-field placeholder="ex) Relu" />
 
-        <v-text><b>KernelInitializer</b></v-text>
-        <v-text-field placeholder="ex) varianceScaling"/>
+        <v-text>
+          <b>KernelInitializer</b>
+        </v-text>
+        <v-text-field placeholder="ex) varianceScaling" />
 
-        <v-text><b>Units</b></v-text>
-        <v-text-field placeholder="ex) 10"/>
-
+        <v-text>
+          <b>Units</b>
+        </v-text>
+        <v-text-field placeholder="ex) 10" />
       </div>
     </div>
-    <v-col class="buttons" cols="12" align="center">
-      <v-btn class="apply" rounded color="#81C784" @click="apply()" >Apply</v-btn>
-      <v-btn class="reset" rounded color="#EF9A9A" @click="reset()">Reset</v-btn>
-    </v-col>
-  </v-navigation-drawer>
+  </v-list>
 </template>
 
 <script>
@@ -45,11 +55,11 @@ export default {
   methods: {
     apply: function() {
       //apply input layer value
-     // TODO: 입력된 파라미터값을 model params값에 넣게 하기
+      // TODO: 입력된 파라미터값을 model params값에 넣게 하기
     },
     reset: function() {
       //reset input layer value
-      // TODO: DOM에 입력된 파라미터값 초기화 
+      // TODO: DOM에 입력된 파라미터값 초기화
     }
   }
 };
