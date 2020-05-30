80158868fcabf8c0676436f7927e9dcc7765fd75
jinsung_cho <cjs97111@gmail.com>
2020-05-31 00:41:21 +0900

    Update grid applied

--- src/views/Board.vue
+++ src/views/Board.vue
@@ -1,70 +1,94 @@
 <template>
   <v-content class="text-left">
-    <v-tabs
-      class="tabbar"
-      background-color="#B0BEC5"
-      color="#000000"
-      dark
-      show-arrows
-      center-active
-      height="48px"
-    >
-      <v-tabs-slider color="#263238" />
-      <v-tab
-        v-for="tabs in tabs"
-        :key="tabs.name"
-        :href="tabs.lnk"
-        @click="renameTitle(tabs.id)"
-      >
-        {{ tabs.name }}
-        <v-btn class="closeTab" icon x-small @click="deleteTabs(tabs)">
-          <v-icon size="small">mdi-close</v-icon>
-        </v-btn>
-      </v-tab>
-      <v-btn class="plustabs" icon @click="addTabs">
-        <v-icon color="white">mdi-plus</v-icon>
-      </v-btn>
-    </v-tabs>
+    <v-card>
+      <v-tabs v-model="tab" height="48px" background-color="#5A6D76" centered dark icons-and-text>
+        <v-tabs-slider></v-tabs-slider>
+        <v-tab v-for="item in items" :key="item" @click="setCompoState(item)">{{ item }}</v-tab>
 
-
-<!-- TODO: Vuex 사용해서 상태에 따라 board / Train / Evaluation 화면 뜨게 하기 -->
-<!-- Vuex에 있는 CompoState 변수 사용 -->
-    <!-- <template v-if="store.status.name == board"> -->
-      <block />
-    <!-- </template> -->
-
-    <!-- <template v-if="store.status.name == train">
-      <train />
-    </template> -->
-
-    <!-- <template v-if="store.status.name == evalutation"> -->
-      <!-- <evaluation /> -->
-    <!-- </template> -->
-
-    <palette />
-    <parameter />
+        <v-tab-item v-model="tab">
+          <v-container>
+            <v-row>
+              <v-col cols="2">
+                <palette />
+              </v-col>
+              <v-col dense cols="8">
+                <v-container>
+                  <v-row>
+                    <v-tabs
+                      class="tabbar"
+                      background-color="#B0BEC5"
+                      color="#000000"
+                      dark
+                      show-arrows
+                      center-active
+                      height="48px"
+                    >
+                      <v-tabs-slider color="#263238" />
+                      <v-tab
+                        v-for="tabs in tabs"
+                        :key="tabs.name"
+                        :href="tabs.lnk"
+                        @click="renameTitle(tabs.id)"
+                      >
+                        {{ tabs.name }}
+                        <v-btn class="closeTab" icon x-small @click="deleteTabs(tabs)">
+                          <v-icon left size="small">mdi-close</v-icon>
+                        </v-btn>
+                      </v-tab>
+                      <v-btn class="plustabs" icon @click="addTabs">
+                        <v-icon color="white">mdi-plus</v-icon>
+                      </v-btn>
+                    </v-tabs>
+                  </v-row>
+                  <v-row dense justify="center">
+                    <block />
+                  </v-row>
+                </v-container>
+              </v-col>
+              <v-col cols="2">
+                <parameter />
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-tab-item>
+        <v-tab-item>
+          <train />
+        </v-tab-item>
+        <v-tab-item>
+          <evaluation />
+        </v-tab-item>
+      </v-tabs>
+      <!-- <v-tab-item v-model="tab">
+        <template>
+          <v-tab-item v-for="item in items" :key="item"></v-tab-item>
+        </template>
+      </v-tab-item>-->
+    </v-card>
   </v-content>
 </template>
 
 <script>
+import block from "@/components/board/Block";
+import train from "@/components/board/Train";
+import evaluation from "@/components/board/Evaluation";
 import palette from "@/components/board/Palette";
 import parameter from "@/components/board/LayerParameter";
-import block from "@/components/board/Block";
-// import train from "@/components/board/Train";
-// import evaluation from "@/components/board/Evaluation"
 
 export default {
   name: "Board",
   components: {
-    palette,
-    parameter,
     block,
-    // train,
-    // evaluation
+    train,
+    evaluation,
+    palette,
+    parameter
   },
   data() {
     return {
       tabs: [{ name: "board 1", lnk: "", id: "1" }],
+      items: ["board", "train", "evaluation"],
+      more: [{ name: "train" }, { name: "test" }],
+      compo: this.$store.getters.getCompoState
     };
   },
   methods: {
@@ -72,7 +96,15 @@ export default {
       this.tabs.push({
         name: `board ${this.tabs.length + 1}`,
         lnk: "",
-        id: `${this.tabs.length + 1}`,
+        id: `${this.tabs.length + 1}`
+      });
+    },
+    addItem(item) {
+      const removed = this.items.splice(0, 1);
+      this.items.push(...this.more.splice(this.more.indexOf(item), 1));
+      this.more.push(...removed);
+      this.$nextTick(() => {
+        this.currentItem = "tab-" + item;
       });
     },
     deleteTabs: function(tab) {
@@ -81,7 +113,15 @@ export default {
     renameTitle: function(tabs) {
       console.log(tabs);
     },
+    setCompoState: function(item) {
+      this.$store.commit("setCompo", item);
+    }
   },
+  computed: {
+    compoState() {
+      return this.$store.getters.getCompoState;
+    }
+  }
 };
 </script>
 
