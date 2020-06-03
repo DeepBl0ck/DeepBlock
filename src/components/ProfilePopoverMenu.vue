dba6d4848079434a417ca0d721074e6062ff7157
LucyHorng <khmin09015@gmail.com>
2020-06-03 20:43:11 +0900

    add: profilePopoverMenue 폼, 프로필 편집 axios 구현

--- src/components/ProfilePopoverMenu.vue
+++ src/components/ProfilePopoverMenu.vue
@@ -1,43 +1,82 @@
 <template>
   <div class="text-center">
-    <v-menu v-model="menu" offset-y :close-on-content-click="false">
+    <v-menu left v-model="menu" offset-y :close-on-content-click="false">
       <template v-slot:activator="{ on }">
         <v-avatar size="27" v-on="on">
           <v-img src="../assets/lucy.jpg" style="cursor:pointer"></v-img>
         </v-avatar>
       </template>
 
-      <v-card>
-        <v-list>
-          <v-list-item>
-            <v-list-item-avatar>
-              <img src="../assets/lucy.jpg" alt="lucy" />
-            </v-list-item-avatar>
-            <v-list-item-content>
-              <v-list-item-title>Lucy</v-list-item-title>
-              <v-list-item-subtitle>khmin09015@gmail.com</v-list-item-subtitle>
-            </v-list-item-content>
+      <v-card class="useravatar" outlined>
+        <div align="center">
+          <v-avatar size="70" @click="editProfile = !editProfile">
+            <v-img src="../assets/lucy.jpg" style="cursor:pointer"></v-img>
+          </v-avatar>
 
-            <v-list-item-action>
-              <v-btn :class="fav ? 'red--text' : ''" icon @click="fav = !fav">
-                <v-icon>mdi-heart</v-icon>
-              </v-btn>
-            </v-list-item-action>
-          </v-list-item>
-        </v-list>
+          <div class="userdata">
+            <div class="username">{{ username }}</div>
+            <p class="email">{{ email }}</p>
+          </div>
+
+          <v-layout row>
+            <v-flex>
+              <div class="datacount">
+                <h3>6</h3>
+                <p class>Projects</p>
+              </div>
+            </v-flex>
+            <v-flex>
+              <div class="datacount">
+                <h3>26</h3>
+                <p>Datasets</p>
+              </div>
+            </v-flex>
+          </v-layout>
+        </div>
 
         <v-divider></v-divider>
         <v-list>
-          <v-list-item v-for="(item, index) in items" :key="index">
-            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          <v-list-item v-for="menu in menus" :key="menu" :href="menu.route" class="pointerClick">
+            <v-list-item-action>
+              <v-icon small>{{menu.icon}}</v-icon>
+            </v-list-item-action>
+            <v-list-item-content>
+              <v-list-item-title @click="menu.action()">{{ menu.title }}</v-list-item-title>
+            </v-list-item-content>
           </v-list-item>
         </v-list>
       </v-card>
     </v-menu>
+
+    <input
+      type="file"
+      accept="image/jpeg, image/png"
+      style="display:none"
+      ref="fileInput"
+      id="file"
+      @change="onFilePicked"
+    />
+
+    <v-dialog v-model="editProfile" max-width="300px">
+      <v-card class="mx-auto" max-width="300" tile>
+        <v-list dense>
+          <v-list-item v-for="profile in profiles" :key="profile" :inactive="true">
+            <v-list-item-content>
+              <v-list-item-title class="pointerClick" @click="profile.action()">
+                {{
+                profile.title
+                }}
+              </v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-card>
+    </v-dialog>
   </div>
 </template>
 
 <script>
+import Swal from "sweetalert2";
 export default {
   props: {
     menu: {
@@ -47,16 +86,121 @@ export default {
   data() {
     return {
       fav: true,
-      items: [
-        { title: "Click Me1" },
-        { title: "Click Me1" },
-        { title: "Click Me1" },
-        { title: "Click Me1" }
+      username: "LucyHorang",
+      email: "kimchi0090@gmail.com",
+      editProfile: false,
+      menus: [
+        {
+          title: "비밀번호 변경",
+          icon: "mdi-lock",
+          route: "/checkPassword"
+        },
+        {
+          title: "로그아웃",
+          icon: "mdi-logout",
+          action: this.logout
+        }
+      ],
+      profiles: [
+        {
+          title: "프로필 업로드",
+          action: this.openDialog
+        },
+        {
+          title: "프로필 삭제"
+        }
       ]
     };
+  },
+  methods: {
+    logout() {
+      this.$axios
+        .delete(`./u/logout`)
+        .then(response => {
+          if (response.status === 200) {
+            location.href = "./";
+          }
+        })
+        .catch(err => {
+          if (err.response.status === 409) {
+            Swal.fire({
+              icon: "error",
+              title: "Sorry....",
+              text: err.response.data.message
+            });
+            location.href = "./";
+          }
+        });
+    },
+    openDialog() {
+      //TODO: pick profile and upload to server
+      return new Promise(() => {
+        this.$refs.fileInput.click();
+      });
+    },
+    onFilePicked() {
+      let data = new FormData();
+      let file = this.$refs.fileInput.files[0];
+
+      data.append("name", "filename");
+      data.append("avatar", file);
+      let config = {
+        header: {
+          ContentType: "multipart/form-data"
+        }
+      };
+      this.$axios
+        .put("/u/avatar", data, config)
+        .then(response => {
+          if (response.status === 200) {
+            Swal.fire({
+              icon: "success",
+              title: "Congratulation",
+              text: response.data.message
+            });
+            location.href = "./profile";
+          }
+        })
+        .catch(err => {
+          if (err.response.status === 401)
+            Swal.fire({
+              icon: "error",
+              title: "Oops...",
+              text: err.response.data.message
+            });
+        })
+        .catch(err => {
+          if (err.response.status === 500)
+            Swal.fire({
+              icon: "error",
+              title: "Oops...",
+              text: err.response.data.message
+            });
+        });
+    }
   }
-};
+}
 </script>
 
-<style>
+<style lang="sass">
+.useravatar
+  padding: 30px 10px 10px 10px
+
+.userinfo
+  padding-top: 100px
+
+.username
+  margin-top: 10px
+  font-weight: bold 
+
+.email
+  font-size: 13px
+
+.userdata
+  padding-top: 5px
+
+.datacount
+  width: 120px
+  height: 100%
+
 </style>
