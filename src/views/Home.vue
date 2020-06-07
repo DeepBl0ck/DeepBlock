3bac67eb9ac7ce9e0e30bcbb6aa449755d1d6420
LucyHorng <khmin09015@gmail.com>
2020-06-07 19:15:04 +0900

    Add : avatar axios 구현 테스트 in Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,8 @@
 <template>
   <v-content>
     <h1>{{ text }}</h1>
-    <img :src="changeImg(dog)" />
-    <v-btn @click="dog = !dog">change IMG</v-btn>
+
+    <v-btn @click="getAvatar">change IMG</v-btn>
   </v-content>
 </template>
 
@@ -12,18 +12,8 @@ export default {
   components: {},
   data() {
     return {
-      text: "This is Home page.",
-      dog: true
+      text: "This is Home page."
     };
-  },
-  methods: {
-    changeImg: function(dog) {
-      if (dog) {
-        return "https://media1.tenor.com/images/bcbb279dee67e5a9d6a22238977a1591/tenor.gif?itemid=12759384";
-      } else {
-        return "https://i.pinimg.com/originals/06/ff/53/06ff53c749e73112fee56c3cf9d5f606.gif";
-      }
-    }
   }
 };
 </script>
