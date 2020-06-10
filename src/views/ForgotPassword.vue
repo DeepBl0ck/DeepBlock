0387591d5d8261bae51802572cf55d3acdb1f8a8
LucyHorng <khmin09015@gmail.com>
2020-06-10 20:51:21 +0900

    Edit: 카드 컴포넌트화(FieldCard) 구현 in ForgotPAssword.vue

--- src/views/ForgotPassword.vue
+++ src/views/ForgotPassword.vue
@@ -1,83 +1,52 @@
 <template>
   <v-content>
-    <v-container class="fill-height" fluid>
-      <v-row align="center" justify="center">
-        <v-col cols="12">
-          <v-card class="forgotForm" icon max-width="400">
-            <v-list-item-title class="passwordTitle">
-              <div class="passwordIconHeadline">
-                <v-icon large>mdi-view-headline</v-icon>DeepBlock
-              </div>
-            </v-list-item-title>
-            <v-divider color="#3949AB"></v-divider>
+    <fieldcard>
+      <v-card-text class="passwordText" style="color: #3949AB;">PASSWORD RECOVER</v-card-text>
+      <v-text class="findPasswordText">Enter your email to reset your password</v-text>
 
-            <v-card-text class="passwordText" style="color: #3949AB;"
-              >PASSWORD RECOVER</v-card-text
-            >
-            <v-text class="findPasswordText"
-              >Enter your email to reset your password</v-text
-            >
-
-            <v-form class="passwordForm">
-              <v-text-field
-              v-model="username"
-                id="username"
-                label="Username"
-                :rules="usernameRules"
-                outlined
-                dense
-              ></v-text-field>
-              <v-text-field
-              v-model="email"
-                id="email"
-                label="Email"
-                :rules="emailRules"
-                outlined
-                dense
-              ></v-text-field>
-              <v-btn @click="submit()" block dark color="indigo"
-                >Send email
-              </v-btn>
-              <v-layout justify-space-between class="passwordLinkLayout">
-                <span
-                  class="loginRouter"
-                  @click="$router.push({ name: 'Login' })"
-                  >Return to login</span
-                >
-                <span
-                  class="signupRouter"
-                  @click="$router.push({ name: 'SignUp' })"
-                  >Sign Up</span
-                >
-              </v-layout>
-            </v-form>
-          </v-card>
-        </v-col>
-      </v-row>
-    </v-container>
+      <v-form class="passwordForm">
+        <v-text-field
+          v-model="username"
+          id="username"
+          label="Username"
+          :rules="usernameRules"
+          outlined
+          dense
+        ></v-text-field>
+        <v-text-field v-model="email" id="email" label="Email" :rules="emailRules" outlined dense></v-text-field>
+        <v-btn @click="submit()" block dark color="indigo">Send email</v-btn>
+        <v-layout justify-space-between class="passwordLinkLayout">
+          <span class="loginRouter" @click="$router.push({ name: 'Login' })">Return to login</span>
+          <span class="signupRouter" @click="$router.push({ name: 'SignUp' })">Sign Up</span>
+        </v-layout>
+      </v-form>
+    </fieldcard>
   </v-content>
 </template>
 
 <script>
+import FieldCard from "../components/user/FieldCard.vue"
 import Swal from "sweetalert2";
 
 export default {
+  components: {
+    fieldcard: FieldCard
+  },
   data() {
     return {
       email: "",
       username: "",
       usernameRules: [
-        (v) => !!v || "UserName is required",
-        (v) =>
-          (v && v.length >= 6) || "UserName must be more than 6 characters",
-        (v) =>
+        v => !!v || "UserName is required",
+        v => (v && v.length >= 6) || "UserName must be more than 6 characters",
+        v =>
           (v && v.length <= 12) || "UserName must be less than 12 characters",
-        (v) => /^[a-z0-9_.]/.test(v) || "only lowercase, _, . can be used",
+        v => /^[a-z0-9_.]/.test(v) || "only lowercase, _, . can be used"
       ],
       emailRules: [
-        (v) => !!v || "E-mail is required",
-        (v) => /.+@.+\..+/.test(v) || "E-mail must be valid",
-      ],
+        v => !!v || "E-mail is required",
+        v => /.+@.+\..+/.test(v) || "E-mail must be valid"
+      ]
     };
   },
   methods: {
@@ -85,16 +54,16 @@ export default {
       this.$axios
         .put(`./findpasswd`, {
           username: this.username,
-          email: this.email,
+          email: this.email
         })
-        .then((res) => {
+        .then(res => {
           if (res.status === 200) {
             Swal.fire({
               icon: "success",
               title: "Email send Chack Your E-mail",
-              text: res.data.message,
+              text: res.data.message
             });
-            location.href = "./login";
+            this.$router.replace("./login");
           }
         })
         .catch(err => {
@@ -107,10 +76,10 @@ export default {
             icon: "error",
             text: msg
           });
-          this.$router.replace('./forgotPassword');
+          this.$router.replace("./forgotPassword");
         });
-    },
-  },
+    }
+  }
 };
 </script>
 
@@ -131,7 +100,7 @@ export default {
   width: 95%
 
 .passwordTitle
-  font-size:1.5em
+  font-size: 1.5em
   color: #3949AB
 
 .passwordIconHeadline
@@ -139,11 +108,11 @@ export default {
   padding-bottom: 10px
 
 .passwordText
-  font-size:1.3em
+  font-size: 1.3em
   padding: 50px 0px 20px 0px
 
 .findPasswordText
-  font-size:0.97em
+  font-size: 0.97em
   padding-top: 5px
 
 .passwordForm
