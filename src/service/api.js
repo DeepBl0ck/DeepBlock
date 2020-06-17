68887f391df9a00932259e721079fb924a207587
Jisub Kim <plumssi9@gmail.com>
2020-06-18 03:19:39 +0900

    add api.js and axios interceptor

---
+++ src/service/api.js
@@ -0,0 +1,40 @@
+import axios from "axios";
+import Swal from "sweetalert2";
+
+const api = axios.create({
+	baseURL: "http://15.165.165.54:8001/api",
+	timeout: 5000,
+	withCredentials: true,
+	headers: { "X-Requested-With": "XMLHttpRequest" },
+});
+
+api.interceptors.request.use(
+	config => {
+		const { token } = JSON.parse(localStorage.userinfo).auth;
+		if (token) config.headers['x-access-token'] = `${token}`
+		return config;
+	},
+	err => Promise.reject(err)
+);
+
+api.interceptors.response.use((res) => {
+	return res;
+}, (err) => {
+	if (!!err.response && err.response.status === 403) {
+		console.log(`service::api catch 403`)
+		Swal.fire({
+			title: "Session Expired",
+			text: "Your session has expired. Would you like to be redirected to the login page?",
+			type: "warning",
+			showCancelButton: true,
+			confirmButtonColor: "#DD6B55",
+			confirmButtonText: "Yes",
+			closeOnConfirm: false
+		}, function () {
+			location.href = '/login';
+		});
+	} else {
+		return Promise.reject(err);
+	}
+});
+export default api;
