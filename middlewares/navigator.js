a9913c6d78cabc3d8ebe756fa2425f8bf87a6355
rootmo-kim <rootmo96@gmail.com>
2020-06-18 09:45:15 +0900

    Update: rename parameter

--- middlewares/navigator.js
+++ middlewares/navigator.js
@@ -43,10 +43,10 @@ module.exports = {
   avatar(req, res, next) {
     const hashed_id = crypto
       .createHash("sha256")
-      .update(req.session.username + salt)
+      .update(req.session_name + salt)
       .digest("hex");
     req.profile_path = `${path.storage}/${hashed_id}/${path.profile}/`;
-    req.profile_name = req.session.username;
+    req.profile_name = req.session_name;
 
     return next();
   },
