8cf3a4a9bde4126d1765b9418bea8f5baa84878a
devsungmin <kimsungmin.dev@gmail.com>
2020-06-21 17:56:54 +0900

    Add: add  Vue MQ

--- src/main.js
+++ src/main.js
@@ -1,35 +1,46 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import vuetify from './plugins/vuetify';
-import Grid from "vue-js-grid"
-import VueInputAutowidth from 'vue-input-autowidth'
-import axios from 'axios'
-import VueAxios from 'vue-axios'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import vuetify from "./plugins/vuetify";
+import Grid from "vue-js-grid";
+import VueInputAutowidth from "vue-input-autowidth";
+import axios from "axios";
+import VueAxios from "vue-axios";
 import "hchs-vue-charts";
 import "chart.js";
+import VueMq from "vue-mq";
 
-Vue.use(VueAxios, axios)
-Vue.use(VueInputAutowidth)
+Vue.use(VueAxios, axios);
+Vue.use(VueInputAutowidth);
 Vue.use(Grid);
-Vue.use(window.VueCharts)
-Vue.config.productionTip = false
+Vue.use(window.VueCharts);
+Vue.config.productionTip = false;
 
 // 자식들간 데이터을 주고 받기 위한 eventBus
-export const eventBus = new Vue()
+export const eventBus = new Vue();
+
+Vue.use(VueMq, {
+  breakpoints: {
+    xs: 600,
+    sm: 960,
+    md: 1264,
+    lg: 1904,
+    xl: Infinity,
+  },
+});
 
 Vue.prototype.$axios = axios.create({
   baseURL: "http://15.165.165.54:8000/api",
   timeout: 5000,
   withCredentials: true,
-  headers: { 'X-Requested-With': 'XMLHttpRequest' }
-})
+  headers: { "X-Requested-With": "XMLHttpRequest" },
+});
 // Vue.prototype.$axios.defaults.headers.common['Access-Token'] = localStorage.getItme('token')
 
 new Vue({
   router,
   store,
   vuetify,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
