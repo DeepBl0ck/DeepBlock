d08f618fb4543a3c97c2571ef3c062fe7b88fb75
LucyHorang <khmin09015@gmail.com>
2020-06-24 19:38:44 +0900

    FIX: Appbars design 수정 in Nevbar.vue

--- src/components/Nevbar.vue
+++ src/components/Nevbar.vue
@@ -1,54 +1,31 @@
 <template>
-  <v-content>
-    <v-navigation-drawer v-model="drawer" app clipped v-if="isLoggedin">
-      <v-list nav>
-        <v-col cols="auto">
-          <v-list-item>
-            <v-list-item-avatar size="50" two-line>
-              <v-img :src="avatar"></v-img>
-            </v-list-item-avatar>
-            <v-list-item-content class="profile">
-              <v-list-item-title class="username">{{ username }}</v-list-item-title>
-              <v-list-item-subtitle class="email">{{ email }}</v-list-item-subtitle>
-            </v-list-item-content>
-          </v-list-item>
-        </v-col>
-
-        <v-divider />
-
-        <template v-for="item in items">
-          <v-list-item :key="item.text" @click="$router.push(item.route)">
-            <v-list-item-action>
-              <v-icon>{{ item.icon }}</v-icon>
-            </v-list-item-action>
-
-            <v-list-item-content>
-              <v-list-item-title style="text-align:left">{{item.text}}</v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-        </template>
-      </v-list>
-    </v-navigation-drawer>
-
-    <!-- appbar -->
-    <v-app-bar dense app clipped-left clipped-right>
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <a style="text-decoration: none" @click="goHome()">
-        <v-toolbar-title class="text-uppercase grey--text">
-          <div>DeepBlock</div>
-        </v-toolbar-title>
-      </a>
+  <div>
+    <v-app-bar app height="70px" color="white" elevate-on-scroll class="appbar-interval auto">
+      <div class="logo">
+        <span class="logo-deep pointer" @click="goHome()">DEEP</span>
+        <span class="logo-block pointer">BLOCK</span>
+      </div>
       <v-spacer />
+      <span
+        class="app-item font-hover pointer"
+        @click="$router.push('/projectMain')"
+        :class="this.$route.path=='/projectMain'?'highlight':''"
+      >Projects</span>
+      <span
+        class="app-item font-hover pointer"
+        @click="$router.push('/datasetMain')"
+        :class="this.$route.path=='/datasetMain'?'highlight':''"
+      >Datasets</span>
 
-      <!-- if logged in -->
-      <template v-if="isLoggedin">
-        <popover :menu="menu" />
-      </template>
+      <div class="item-border">
+        <template v-if="isLoggedin">
+          <popover :menu="menu" />
+        </template>
 
-      <!-- not logged in -->
-      <template v-else>
-        <v-btn outlined @click="$router.push('/login')" style="margin-left:10px">Login</v-btn>
-      </template>
+        <template v-else>
+          <v-btn outlined color="#1e5f8d" @click="$router.push('/login')">Login</v-btn>
+        </template>
+      </div>
     </v-app-bar>
 
     <v-dialog v-model="addProject" max-width="500px">
@@ -64,7 +41,7 @@
         </v-card-actions>
       </v-card>
     </v-dialog>
-  </v-content>
+  </div>
 </template>
 
 <script>
@@ -92,32 +69,26 @@ export default {
     };
   },
   methods: {
-    goHome: function () { 
-      this.$router.push("/")
-      .catch(err => {
-        if ( err.name !== "NavigationDuplicated" )
-          throw err
-      })
+    goHome: function() {
+      this.$router.push("/").catch(err => {
+        if (err.name !== "NavigationDuplicated") throw err;
+      });
     },
-    goProfile(){
-      this.$router.push("/profile")
-      .catch(err => {
-        if ( err.name !== "NavigationDuplicated" )
-          throw err
-      })
+    goProfile() {
+      this.$router.push("/profile").catch(err => {
+        if (err.name !== "NavigationDuplicated") throw err;
+      });
     },
-    goSetting(){
-      this.$router.push("/model")
-      .catch(err => {
-        if ( err.name !== "NavigationDuplicated" )
-          throw err
-      })
+    goSetting() {
+      this.$router.push("/model").catch(err => {
+        if (err.name !== "NavigationDuplicated") throw err;
+      });
     }
   },
   computed: {
     ...mapGetters("auth", ["isLoggedin", "username", "email"]),
-    ...mapGetters("avatar", ["avatar"])
-  }
+    ...mapGetters("avatar", ["avatar"]),
+  },
 };
 </script>
 
@@ -134,12 +105,42 @@ export default {
 #nav
   padding: 30px
 
-
 #nav a
   font-weight: bold
   color: #2c3e50
 
-
 #nav a.router-link-exact-active
   color: #42b983
+
+.appbar-interval
+  padding-left: 50px
+  padding-right: 50px
+  
+.logo-deep
+  font-size: 32px
+  color: green
+
+.logo-block
+  font-size: 32px
+  font-weight: bold
+  color: #1e5f8d
+
+.app-item
+  padding-right: 15px
+  font-weight: bold
+  color: grey
+
+.font-hover
+  &:hover
+    color: #1e5f8d
+
+.highlight
+    color: #1e5f8d
+
+.item-border
+  border-left: 2px solid #1e5f8d
+  padding-left: 15px
+
+.pointer
+  cursor: pointer
 </style>
