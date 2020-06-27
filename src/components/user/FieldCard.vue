d9854ede2fe95317e98d51afa6320964adfc37a3
LucyHorang <khmin09015@gmail.com>
2020-06-28 00:52:44 +0900

    FIX: css 추가 구현 및 클래스명 수정

--- src/components/user/FieldCard.vue
+++ src/components/user/FieldCard.vue
@@ -1,14 +1,17 @@
 <template>
   <v-content>
     <v-container>
-      <div class="d-flex flex-wrap justify-center">
-          <v-card class="centeralize" min-width="400" max-width="400" outlined>
-            <v-list-item-title class="project-title">DEEPBLOCK
-            </v-list-item-title>
-            <v-divider color="#1e5f8d"></v-divider>
-            <slot></slot>
-          </v-card>
+      <div id="card-area" class="d-flex flex-wrap justify-center align-center">
+        <v-card class="centeralize" min-width="400" max-width="400" outlined>
+          <div style="padding-top: 10px;
+    padding-bottom: 10px">
+            <span class="lightorange-color deep-font-size">DEEP</span>
+            <span class="darkblue-color block-font-size block-font-bold">BLOCK</span>
           </div>
+          <v-divider class="darkblue-color"></v-divider>
+          <slot></slot>
+        </v-card>
+      </div>
     </v-container>
   </v-content>
 </template>
@@ -23,14 +26,19 @@ export default {
   },
   data() {
     return {};
-  },
+  }
 };
 </script>
 
-<style lang="sass" scoped>
-.project-title
-    font-size: 1.5em
-    color: #1e5f8d
-    padding-top: 10px
-    padding-bottom: 10px
+<style lang="sass">
+.deep-font-size, .block-font-size
+  font-size: 1.5em
+
+.block-font-bold
+  font-weight: bold
+
+#card-area
+  .theme--light.v-divider 
+    border-color: #274555
+
 </style>
